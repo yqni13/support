6333d0dba2f6e6fd7e1be7a8660c2ccc8ad062dc
yqni13 <yqni13@protonmail.com>
2025-08-14 23:33:13 +0200

    build: add packages + env variables

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const { ExpressLoader } = require('./loaders/express.loader');
+const { RoutesLoader } = require('./loaders/routes.loader');
+const { MiddlewareLoader } = require('./loaders/middleware.loader');
+const DBConnection = require('./configs/db');
+
+const app = ExpressLoader.init();
+const version = 'v1';
+
+RoutesLoader.initRoutes(app, version);
+MiddlewareLoader.init(app);
+const dbConnect = new DBConnection();
+dbConnect.init();
+
+module.exports = app;
