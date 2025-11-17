c2e23113e66f0b218e3612d77ea4a554f088a777
yqni13 <yqni13@protonmail.com>
2025-11-17 18:55:13 +0100

    test: add 'users' validator/models tests

---
+++ backend/src/dtos/users.dto.ts
@@ -0,0 +1,17 @@
+import { UserStatus } from "../utils/enums/user-status.enum";
+import { Flag } from "../utils/enums/flag.enum";
+
+export interface UsersCreateUpdateDTO {
+    email: string,
+    status: UserStatus,
+    flag: Flag | null
+}
+
+export interface UsersResponseDTO {
+    user_id: string,
+    email: string,
+    status: UserStatus,
+    flag: Flag | null,
+    last_modified: string,
+    created_on: string
+}
--- backend/tests/unit-tests/validators/common.validators.test.ts
+++ backend/tests/unit-tests/validators/common.validators.test.ts
@@ -3,6 +3,8 @@ import * as CustomValidators from "../../../src/utils/customValidator.utils";
 import { EnvMode } from "../../../src/utils/enums/env-mode.enum";
 import { MailSource } from "../../../src/utils/enums/mail-source.enum";
 import { ApiKeyStatus } from "../../../src/utils/enums/api-key-status.enum";
+import { UserStatus } from "../../../src/utils/enums/user-status.enum";
+import { Flag } from "../../../src/utils/enums/flag.enum";
 
 describe('CustomValidator tests, priority: no model specification', () => {
 
@@ -64,20 +66,32 @@ describe('CustomValidator tests, priority: no model specification', () => {
             })
         })
 
-        test('fn: validateEmail, params: <email> = user@test.com', () => {
-            const mockParam_email = 'user@test.com';
-            const testFn = CustomValidators.validateEmail(mockParam_email);
+        test('fn: validateEnum, params <enumObj> = UserStatus', () => {
+            const mockParam_enumObj = UserStatus;
+            const mockParam_enumName = 'userStatus';
             const expectResult = true;
 
-            expect(testFn).toBe(expectResult);
+            Object.values(mockParam_enumObj).forEach((value) => {
+                expect(CustomValidators.validateEnum(
+                    value,
+                    mockParam_enumObj,
+                    mockParam_enumName
+                )).toBe(expectResult);
+            })
         })
 
-        test('fn: validateEmail, params: <email> = user@test.co.at', () => {
-            const mockParam_email = 'user@test.co.at';
-            const testFn = CustomValidators.validateEmail(mockParam_email);
+        test('fn: validateEnum, params <enumObj> = Flag', () => {
+            const mockParam_enumObj = Flag;
+            const mockParam_enumName = 'flag';
             const expectResult = true;
 
-            expect(testFn).toBe(expectResult);
+            Object.values(mockParam_enumObj).forEach((value) => {
+                expect(CustomValidators.validateEnum(
+                    value,
+                    mockParam_enumObj,
+                    mockParam_enumName
+                )).toBe(expectResult);
+            })
         })
     })
 
@@ -110,173 +124,6 @@ describe('CustomValidator tests, priority: no model specification', () => {
                     mockParam_enumObj,
                     mockParam_enumName
                 );
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmail, params: invalid <email> by number of "@"', () => {
-            const mockParam_email = 'user@@test.com';
-            const expectResult = 'support-invalid-length#email<@>$1';
-
-            expect(() => {
-                CustomValidators.validateEmail(mockParam_email);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by total length (min)', () => {
-            const mockParam_email = 'a@b';
-            const mockParam_posATsign = 1;
-            const expectResult = 'support-invalid-min#email?6';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by total length (max)', () => {
-            const mockParam_email = 'Iam70characterslongAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA@ToExpandOn.TotalOf400CharactersBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB';
-            const mockParam_posATsign = 71;
-            const expectResult = 'support-invalid-max#email!320';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by length of username (min)', () => {
-            const mockParam_email = '@test.com';
-            const mockParam_posATsign = 0;
-            const expectResult = 'support-invalid-length#email-username';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by length of username (max)', () => {
-            const mockParam_email = 'Iam70characterslongAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA@test.com';
-            const mockParam_posATsign = 71;
-            const expectResult = 'support-invalid-length#email-username';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by length of domain (min)', () => {
-            const mockParam_email = 'user@t.c';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-length#email-domain';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by length of domain (max)', () => {
-            const mockParam_email = 'user@Iam270characterslong.BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-length#email-domain';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailLength, params: invalid <email> by length of top level domain (min)', () => {
-            const mockParam_email = 'user@test.c';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-length#email-topleveldomain?2';
-
-            expect(() => {
-                CustomValidators.validateEmailLength(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by username RegEx', () => {
-            const mockParam_email = 'u§er@test.com';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-email#regex-username';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by domain RegEx', () => {
-            const mockParam_email = 'user@te$t.com';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-email#regex-domain';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by domain missing "."', () => {
-            const mockParam_email = 'user@testcom';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-email#regex-domain';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by hyphens positioned before "@"', () => {
-            const mockParam_email = 'user-@test.com';
-            const mockParam_posATsign = 5;
-            const expectResult = 'support-invalid-email#hyphen<@>';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by hyphens positioned after "@"', () => {
-            const mockParam_email = 'user@-test.com';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-email#hyphen<@>';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by mail server RegEx', () => {
-            const mockParam_email = 'user@§.com';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-email#mailserver';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailSyntax, params: invalid <email> by missing mail server', () => {
-            const mockParam_email = 'user@.com';
-            const mockParam_posATsign = 4;
-            const expectResult = 'support-invalid-email#mailserver';
-
-            expect(() => {
-                CustomValidators.validateEmailSyntax(mockParam_email, mockParam_posATsign);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailPolicies, params: invalid <email> by forbidden keyword', () => {
-            const mockParam_email = 'noreply@test.com';
-            const expectResult = 'support-invalid-email#keyword:noreply';
-
-            expect(() => {
-                CustomValidators.validateEmailPolicies(mockParam_email);
-            }).toThrow(expectResult);
-        })
-
-        test('fn: validateEmailPolicies, params: invalid <email> by containing empty spaces', () => {
-            const mockParam_email = 'my user@test .com';
-            const expectResult = 'support-invalid-email#keyword:emptyspaces';
-
-            expect(() => {
-                CustomValidators.validateEmailPolicies(mockParam_email);
             }).toThrow(expectResult);
         })
     })
