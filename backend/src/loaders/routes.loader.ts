81afe8eb17be82a6744255163c38da6a24776ffc
yqni13 <yqni13@protonmail.com>
2026-03-25 20:39:22 +0100

    feat: add health endpoints + logic

--- backend/src/loaders/routes.loader.ts
+++ backend/src/loaders/routes.loader.ts
@@ -6,15 +6,17 @@ import usersRouter from '../routes/users.route';
 import testRouter from '../routes/test.route';
 import feedbackRouter from '../routes/feedback.route';
 import feedbackRatingRouter from '../routes/feedback-rating.route';
+import healthRouter from '../routes/health.route';
 
 export class RoutesLoader {
     static initRoutes(app: Application, version: string) {
         app.use(`/api/${version}/clients`, clientsRouter);
         app.use(`/api/${version}/feedback`, feedbackRouter);
         app.use(`/api/${version}/feedback-rating`, feedbackRatingRouter);
+        app.use(`/api/${version}/health`, healthRouter);
         app.use(`/api/${version}/meta`, metaRouter);
         app.use(`/api/${version}/test`, testRouter);
         app.use(`/api/${version}/tickets`, ticketsRouter);
         app.use(`/api/${version}/users`, usersRouter);
-    }
+    };
 }
---
+++ backend/src/routes/health.route.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { awaitHandlerFactory as factory } from '../middleware/awaitHandlerFactory.middleware';
+import healthController from "../controllers/health.controller";
+import { authAdmin } from "../middleware/auth.admin.middleware";
+
+const router = Router();
+
+// public
+router.get(
+    '/',
+    factory(healthController.getHealthCheck)
+);
+
+router.get(
+    '/details',
+    authAdmin(),
+    factory(healthController.getHealthCheckDetails)
+)
+
+export default router;
